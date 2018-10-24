f59f5ebd9cd406596e08a011492841a898652e6b
Saru <anthony.castrov@gmail.com>
2018-10-24 12:29:14 -0500

    add correct comments standar in model Task

--- app/Task.php
+++ app/Task.php
@@ -7,21 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
 
-
-        /*
-         * Static Function
-         */
-        protected static function complete()
+    /**
+     * Get all completed tasks
+     *
+     * @return mixed
+     */
+    protected static function complete()
         {
             return static::where('completed',0)->get();
         }
 
-        /*
-         * Public function
-         * scope preview of the method name (use camelCase)
-         */
-        public function scopeIncomplete($query)
-        {
-            return $query->where('completed',0);
-        }
+
+    /**
+     * Get all "incomplete tasks" and other validation in $query.
+     *
+     * @param $query -> query to execute with the final query
+     * @return mixed
+     */
+    public function scopeIncomplete($query)
+    {
+        return $query->where('completed',0);
+    }
 }
